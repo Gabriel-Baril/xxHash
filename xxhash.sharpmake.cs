6e25ac642179f505efc364776a3815de9b364026
Gabriel Baril <gabriel-baril2010@hotmail.com>
2024-12-28 22:51:44 -0500

    Added sharpmake files

---
+++ xxhash.sharpmake.cs
@@ -0,0 +1,26 @@
+using System.IO; // For Path.Combine
+using Sharpmake; // Contains the entire Sharpmake object library.
+
+[Generate]
+public class XXHashProject : BaseCppProject
+{
+    public XXHashProject()
+    {
+        Name = "xxhash";
+        SourceRootPath = @"[project.SharpmakeCsPath]\src";
+        AddTargets(TargetUtil.DefaultTarget);
+    }
+
+    [Configure]
+    public new void ConfigureAll(Project.Configuration conf, Target target)
+    {
+        base.ConfigureAll(conf, target);
+
+        conf.SolutionFolder = Constants.EXTERNAL_VS_CATEGORY;
+
+        conf.Output = Project.Configuration.OutputType.Lib;
+        conf.TargetPath = @"[project.SharpmakeCsPath]\out\bin\[target.Platform]-[target.Optimization]";
+        conf.IntermediatePath = @"[project.SharpmakeCsPath]\out\intermediate\[target.Platform]-[target.Optimization]";
+        conf.IncludePaths.Add(@"[project.SharpmakeCsPath]\src");
+    }
+}
